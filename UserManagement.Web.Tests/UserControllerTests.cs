747f007584aad68833ca6edc9ed137e2073df420
Tom Fletcher <tom@tomfletcher.tech>
2026-09-09 23:55:42 +0100

    Task 1: Update/Add additional tests to cover new User Filter behaviour

--- UserManagement.Web.Tests/UserControllerTests.cs
+++ UserManagement.Web.Tests/UserControllerTests.cs
@@ -1,4 +1,6 @@
+using Microsoft.AspNetCore.Mvc;
 using UserManagement.Models;
+using UserManagement.Services.Domain.Models;
 using UserManagement.Services.Domain.Interfaces;
 using UserManagement.Web.Models.Users;
 using UserManagement.WebMS.Controllers;
@@ -15,12 +17,51 @@ public class UserControllerTests
         var users = SetupUsers();
 
         // Act: Invokes the method under test with the arranged parameters.
-        var result = controller.List();
+        var result = controller.List(new());
 
         // Assert: Verifies that the action of the method under test behaves as expected.
-        result.Model
-            .Should().BeOfType<UserListViewModel>()
+        result
+            .Should().BeOfType<ViewResult>()
+            .Which.Model.Should().BeOfType<UserListViewModel>()
             .Which.Items.Should().BeEquivalentTo(users);
+    }
+
+
+    [Fact]
+    public void List_WhenFilterIsInvalid_MustRedirectToUnfilteredList()
+    {
+        // Arrange
+        var controller = CreateController();
+        controller.ModelState.AddModelError(nameof(UserFilter.IsActive), "Invalid");
+
+        // Act
+        var result = controller.List(new());
+
+        // Assert
+        result.Should().BeOfType<RedirectToActionResult>().Which.ActionName.Should().Be(nameof(UsersController.List));
+
+        _userService.Verify(s => s.Filter(It.IsAny<UserFilter>()), Times.Never());
+    }
+
+    [Theory]
+    [InlineData(true)]
+    [InlineData(false)]
+    public void List_WhenFilterProvided_MustPassFilterToService(bool isActive)
+    {
+        // Arrange
+        var controller = CreateController();
+        var users = SetupUsers(isActive: isActive);
+
+        // Act
+        var result = controller.List(new UserFilter { IsActive = isActive });
+
+        // Assert
+        result
+            .Should().BeOfType<ViewResult>()
+            .Which.Model.Should().BeOfType<UserListViewModel>()
+            .Which.Items.Should().BeEquivalentTo(users);
+
+        _userService.Verify(s => s.Filter(It.Is<UserFilter>(f => f.IsActive == isActive)), Times.Once());
     }
 
     private User[] SetupUsers(string forename = "Johnny", string surname = "User", string email = "juser@example.com", bool isActive = true)
@@ -37,7 +78,7 @@ public class UserControllerTests
         };
 
         _userService
-            .Setup(s => s.GetAll())
+            .Setup(s => s.Filter(It.IsAny<UserFilter>()))
             .Returns(users);
 
         return users;
